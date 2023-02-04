ad2ce6ec77ccd41345a298b25cbe492df13090af
Kartik Menon <kartikmenon.2002@gmail.com>
2023-02-04 19:24:19 +0530

    ui

--- components/events/EventCard.js
+++ components/events/EventCard.js
@@ -2,10 +2,12 @@ import { useContext, useEffect, useState } from "react"
 import GenericModal from "../common/GenericModal"
 import { eventListContext } from "../../context/eventsContext"
 import moment from "moment/moment"
+ import { useRouter } from "next/router";
 
 function EventCard(props){
     const [showModal, setModal] = useState(false)
     const [eventDet, setEventDet] = useState({})
+    const router = useRouter();
     const closeHandler=()=>{
         setModal(false)
     }
@@ -48,8 +50,8 @@ function EventCard(props){
 
     // },]
     return(
-        <div>
-            {showModal && <GenericModal closeHandler={closeHandler} isStepModal={false} title={eventDet.name} setTitle={false}>
+        <div className={`${showModal?"h-[70vh] overflow-hidden":""}`}>
+            {showModal && <GenericModal closeHandler={closeHandler} isStepModal={false} setTitle={false}>
                 <div className="px-5 grid grid-rows-2 justify-between w-full">
                     <div>
                         <img src={eventDet.banner} alt="" />
@@ -59,7 +61,10 @@ function EventCard(props){
                             <div className="text-3xl font-semibold">
                                 {eventDet.name}
                             </div>
-                            <div className="text-3xl px-3 font-semibold flex">
+                            <div className="text-3xl px-3 font-semibold flex" onClick={() => {
+                                router.push(`/committee/editevent/${eventDet._id}`)
+                                // console.log("click")
+                            }}>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-5 flex items-center"><path fill="#5F5F5F" d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.8 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/></svg>
                             </div>
                         </div>
---
+++ pages/committee/editevent/[slug].js
@@ -0,0 +1,218 @@
+const { useState, useEffect } = require("react");
+import moment from "moment/moment";
+import DropFileUpload from "../../../components/common/DropFileUpload";
+import DatePickers from "../../../components/common/DatePicker";
+import TimePickers from "../../../components/common/TimePicker";
+import axios from "axios";
+import { storage } from "../../../firebase/firebase";
+ import { useRouter } from "next/router";
+import {
+  getDownloadURL,
+  ref,
+  deleteObject,
+  uploadBytes,
+  getMetadata,
+} from "firebase/storage";
+import {toast} from "react-toastify"
+const Index = () => {
+    const router=useRouter()
+    const [event, setEvent] = useState({})
+    const [name, setName] = useState("");
+  const [des, setDes] = useState("");
+  const [type, setType] = useState("");
+  const [venue, setVenue] = useState("");
+  const [banner,setBanner]=useState("")
+  const [date, setDate] = useState();
+  const [startTime, setStartTime] = useState();
+  const [endTime, setEndTime] = useState();
+    async function getEventById(id){
+        const response = await axios.get(`http://localhost:5000/api/events/?id=${id}`,{},{})
+        console.log(response)
+        setEvent(response.data[0])
+        console.log(event)
+        setName(event.name)
+        setDes(event.description)
+        setType(event.domain)
+        setVenue(event.venue)
+        setBanner(event.banner)
+        setDate(moment(event.date).format("YYYY-MM-DD"))
+        setStartTime(moment(event.startTime).format("hh:mm:ss a"))
+        setEndTime(moment(event.endTime).format("hh:mm:ss a"))
+    }
+    useEffect(() => {
+        const {slug} = router.query
+        if(!slug){
+            return;
+        }
+        getEventById(slug)
+    },[router.query])
+  const [image, setImageArray] = useState([]);
+  const [Uploaded, setIsUploaded] = useState(false);
+  const types = ["Tech", "Non Tech", "Sports", "Seminar", "Webinar"];
+  const getImageURL = async (image) => {
+    try {
+      console.log(image)
+      console.log(storage)
+      const imageRef = ref(
+        storage,
+        `committee/${name}/${image.name}`
+      );
+      const imageUploadResponse = await uploadBytes(imageRef, image);
+      console.log(imageUploadResponse)
+      const imageDownloadResponse = await getDownloadURL(imageRef);
+      console.log(imageDownloadResponse)
+      return imageDownloadResponse;
+    } catch (error) {
+      console.log(error);
+    }}
+  
+  const submitHandler = async(e) => {
+    e.preventDefault();
+   
+    let eventInfo = {
+     
+      name,
+      description: des,
+      date,
+      startTime,
+      endTime,
+      committee:"123",
+      banner,
+      domain:type
+
+    };
+    console.log(eventInfo)
+    const res=await axios.post("http://localhost:5000/api/events",eventInfo).then(()=>{
+      router.push('/committee')
+     
+
+    })
+
+  };
+  const venueHandler=()=>{
+    
+  }
+  const upload = (accepted, rejected) => {
+    setImageArray([...accepted]);
+    setIsUploaded(true);
+  };
+  useEffect(()=>{
+    if(image.length>0)
+  getImageURL(image[image.length-1]).then((banner)=>setBanner(banner))
+  },[image])
+  if(!event){
+    return ;
+  }
+  return (
+    <div className="bg-tertiaryblue-100 text-sm flex flex-col ">
+      <div className="w-[100%] ">
+        <img
+          src="https://images.unsplash.com/photo-1492684223066-81342ee5ff30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+          className="w-[100%] h-[400px] opacity-75"
+        />
+        <p className="translate-y-[-200px] text-white font-bold text-6xl mx-20">
+          Edit Event
+        </p>
+      </div>
+      <form
+        onSubmit={submitHandler}
+        className="flex  justify-center w-[80%]  h-[80%] py-9 px-10 bg-tertiaryblue-100 text-white  font-medium font-display rounded-md opacity-95 "
+      >
+        <div className="flex flex-col w-[50%] mr-20">
+          <label htmlFor="email" className="my-2 ">
+            Event Title<span className="text-red-600">*</span>
+          </label>
+          <input
+            id="name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            className="py-2 bg-tertiaryblue-100 border-b-2 w-[80%] mb-4 border-white text-white focus:outline-none px-1"
+            placeholder="Enter event name"
+          ></input>
+          <label htmlFor="des">
+            Event Description<span className="text-red-600">*</span>
+          </label>
+          <textarea
+            id="des"
+            value={des}
+            onChange={(event) => setDes(event.target.value)}
+            rows={1}
+            placeholder="Enter event description"
+            className="w-[80%] py-2 bg-tertiaryblue-100 border-b-2  mb-4 border-white text-white focus:outline-none px-1"
+          ></textarea>
+          <label htmlFor="type">
+            Event Type<span className="text-red-600">*</span>
+          </label>
+          <select
+            id="type"
+            value={type}
+            onChange={(event) => setType(event.target.value)}
+            className="my-2 bg-tertiaryblue-100 border-b-2 w-[80%] mb-4 border-white text-white focus:outline-none py-2"
+          >
+            <option value="">Select event type</option>
+            {types.map((type, i) => (
+              <option value={type} id={i}>
+                {type}
+              </option>
+            ))}
+          </select>
+          <p className="my-2">
+            Event Banner Image<span className="text-red-600">*</span>
+          </p>
+          <div className="w-[80%]">
+          <DropFileUpload
+            msg={"Drag files to upload or"}
+            extension="browse"
+            uploadFiles={upload}
+          ></DropFileUpload>
+
+          </div>
+         
+          {(image.length>0)?
+            <img src={URL.createObjectURL(image[image.length - 1])}></img>:<img src={banner}></img>}
+          
+           <button
+          type="submit"
+          className="my-7 rounded-md text-white bg-pink-500 w-[25%] py-2 "
+        >
+          Post Event
+        </button>
+        </div>
+        <div className="flex flex-col  w-[40%]">
+          <label htmlFor="type" className="mt-4">
+            Event Date<span className="text-red-600">*</span>
+          </label>
+          <div className="border-b-2 border-white">
+            <DatePickers
+              dateHandler={(date) => setDate(moment(date).format("YYYY-MM-DD"))}
+            ></DatePickers>
+          </div>
+          <label htmlFor="type" className="mt-4">
+            Event Start Time<span className="text-red-600">*</span>
+          </label>
+          <div className="border-b-2 border-white">
+            <TimePickers
+              timeHandler={(startTime) => setStartTime(moment(startTime).format("HH:MM"))}
+            ></TimePickers>
+          </div>
+          <label htmlFor="type" className="mt-4">
+            Event End Time<span className="text-red-600">*</span>
+          </label>
+          <div className="border-b-2 border-white">
+            <TimePickers
+              timeHandler={(endTime) => setEndTime(moment(startTime).format("HH:MM"))}
+            ></TimePickers>
+          </div>
+          <button  className="my-10 bg-tertiarygrey-450 py-3 w-[50%] rounded-sm" type="button" onChange={()=>venueHandler}>
+           Check Venue
+          </button>
+
+        </div>
+      
+      </form>
+      
+    </div>
+    
+  );
+};
+export default Index;
